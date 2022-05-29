082ce7d85d15244ec6039eb2a91f120a925ebc82
YCCD <94218957+YCCDSZXH@users.noreply.github.com>
2022-05-29 02:24:32 +0000

    update requirements.txt

--- Pixiv_v3.py
+++ Pixiv_v3.py
@@ -9,10 +9,9 @@ import aiofiles
 
 
 async def write_data(path, data):
-  # 异步方式执行with操作,修改为 async with
-  async with aiofiles.open(path, "wb") as fp:
-    await fp.write(data)
-
+    # 异步方式执行with操作,修改为 async with
+    async with aiofiles.open(path, "wb") as fp:
+        await fp.write(data)
 
 
 headers = {
@@ -22,66 +21,69 @@ headers = {
 
 
 def getList():
-  url = "https://www.pixiv.net/ranking.php"
-  res = requests.get(url)
-  # print(res.text)
-  res = res.text.translate(str.maketrans({
-      "\n": None,
-      "\r": None,
-      " ": None
-  }))
-  list = re.findall(r'<sectionid="(.*?)</span></a></section>', res)
-  print(len(list))
-  return list
+    url = "https://www.pixiv.net/ranking.php"
+    res = requests.get(url)
+    # print(res.text)
+    res = res.text.translate(str.maketrans({
+        "\n": None,
+        "\r": None,
+        " ": None
+    }))
+    list = re.findall(r'<sectionid="(.*?)</span></a></section>', res)
+    print(len(list))
+    return list
 
 
 def getDetailInfo(relist):
-  allInfoList = []
-  for i in relist:
-    res = re.findall(
-        'data-rank="(.*?)"data-rank-text=".*?"data-title="(.*?)"data-user-name="(.*?)"data-date="(.*?)"data-view-count=".*?"data-rating-count=".*?"data-attr=".*?"data-id="(.*?)"', i)[0]
-    allInfoList.append(res)
-  return allInfoList
+    allInfoList = []
+    for i in relist:
+        res = re.findall(
+            'data-rank="(.*?)"data-rank-text=".*?"data-title="(.*?)"data-user-name="(.*?)"data-date="(.*?)"data-view-count=".*?"data-rating-count=".*?"data-attr=".*?"data-id="(.*?)"', i)[0]
+        allInfoList.append(res)
+    return allInfoList
 
 
 async def downloadSingalImgInfo(PInfo, basePath='OutPut'):
-  url = 'https://www.pixiv.net/en/artworks/' + PInfo[4]
-  async with aiohttp.ClientSession() as session:
-    async with await session.get(url) as resp:
-      res = await resp.text()
-      originImgLink = re.findall(r'"original":"(.*?)"', res)[0]
-      async with await session.get(originImgLink, headers=headers) as resp:
-        await write_data(basePath + str("%03d" % int(PInfo[0]))+f'{PInfo[4]}.jpg', await resp.read())
-        print(f"{PInfo[0]} {PInfo[1]} {PInfo[2]} {PInfo[3]} {PInfo[4]}")
-
+    url = 'https://www.pixiv.net/en/artworks/' + PInfo[4]
+    async with aiohttp.ClientSession() as session:
+        async with await session.get(url) as resp:
+            res = await resp.text()
+            originImgLink = re.findall(r'"original":"(.*?)"', res)[0]
+            async with await session.get(originImgLink, headers=headers) as resp:
+                await write_data(basePath + str("%03d" % int(PInfo[0]))+f'{PInfo[4]}.jpg', await resp.read())
+                print(
+                    f"{PInfo[0]} {PInfo[1]} {PInfo[2]} {PInfo[3]} {PInfo[4]}")
 
 
 def mkdir(path):
-  path = path.strip()
-  path = path.rstrip("\\")
-  isExists = os.path.exists(path)
-  if not isExists:
-    os.makedirs(path)
-    return True
-  else:
-    return False
+    path = path.strip()
+    path = path.rstrip("\\")
+    isExists = os.path.exists(path)
+    if not isExists:
+        os.makedirs(path)
+        return True
+    else:
+        return False
+
 
 def getNowDate():
-  return datetime.datetime.now().strftime('%Y%m%d')
+    return datetime.datetime.now().strftime('%Y%m%d')
+
 
 def main():
-  tasks = []
-  originInfoList = getList()
-  delailInfoList = getDetailInfo(originInfoList)
-  basePath = f"OutPut/{ getNowDate()}/"
-  mkdir(basePath)
-  for i in delailInfoList:
-    tasks.append(asyncio.ensure_future(downloadSingalImgInfo(i, basePath)))
-  loop = asyncio.get_event_loop()
-  loop.run_until_complete(asyncio.wait(tasks))
+    tasks = []
+    originInfoList = getList()
+    delailInfoList = getDetailInfo(originInfoList)
+    basePath = f"OutPut/{ getNowDate()}/"
+    mkdir(basePath)
+    for i in delailInfoList:
+        tasks.append(asyncio.ensure_future(downloadSingalImgInfo(i, basePath)))
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(asyncio.wait(tasks))
+
 
 # 主函数
 if __name__ == "__main__":
-  start = time.time()
-  main()
-  print('耗时', time.time()-start)
+    start = time.time()
+    main()
+    print('耗时', time.time()-start)
